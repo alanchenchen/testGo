01a898f7a402f30d2b31f94385041e1ad919fd15
alanchenchen <739709491@qq.com>
2019-05-31 16:19:55 +0800

    测试

--- main.go
+++ main.go
@@ -1,4 +1,4 @@
-package testGo
+package main
 
 import (
 	"fmt"
@@ -15,6 +15,15 @@ func (a Man) show(msg string) {
 	s := make([]int, 1)
 	s1 := appendInt.AppendInt(s, 1)
 
-	fmt.Println(a.Name)
+	fmt.Println(a.Name, msg)
 	fmt.Println(s1)
+}
+
+func main() {
+	man := Man {
+		Name: "alan",
+		Age: 25,
+	}
+
+	man.show("hello")
 }
